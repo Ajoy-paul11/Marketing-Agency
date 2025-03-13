88911ef8ed909b4cf3677c392b61c6e080e7f5c5
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-03-13 13:10:24 +0530

    Add ScrollToTop component to multiple client pages for improved navigation and layout consistency.

--- src/components/client/Nps.jsx
+++ src/components/client/Nps.jsx
@@ -4,17 +4,19 @@ import business from "../../assets/Magazine/business.png";
 import bigfm from "../../assets/Radio/bigfm.png";
 import facebook from "../../assets/DigitalMedia/facebook.png";
 import economic from "../../assets/Newspaper/economic.jpg";
+import ScrollToTop from "../ScrollToTop.jsx";
 
 function Nps() {
   return (
-    <div className=" py-6 bg-[#f4f5ff]">
-      <div className=" px-4 mb-4">
+    <div className=" py-6 bg-[#f4f5ff] mt-[75px]">
+      <ScrollToTop />
+      <div className=" px-4 mb-4 container mx-auto">
         <h1 className=" pl-2 lg:pl-16 xl:pl-32 text-3xl font-semibold text-primary py-3">
           <span className=" primary-font text-orange-600">NPS School</span>{" "}
           associated with the services:{" "}
         </h1>
       </div>
-      <div className=" grid grid-cols-1 md:gird-cols-2 lg:grid-cols-4 gap-6  px-12 xl:px-48">
+      <div className=" container mx-auto grid grid-cols-1 md:gird-cols-2 lg:grid-cols-3 gap-6  px-12 xl:px-48">
         <div>
           <ClientService
             imageSrc={business}
