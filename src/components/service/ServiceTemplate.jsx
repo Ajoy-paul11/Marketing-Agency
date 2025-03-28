f1d4ebec5af2ab778e84115a16d6d0cb8dadcfcb
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-03-28 14:34:09 +0530

    Add react-lazy-load-image-component for optimized image loading in ServiceTemplate.

--- src/components/service/ServiceTemplate.jsx
+++ src/components/service/ServiceTemplate.jsx
@@ -11,6 +11,8 @@ import {
 } from "@material-tailwind/react";
 import Aos from "aos";
 import "aos/dist/aos.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function ServiceTemplate({ img, title, type, description, price }) {
   useEffect(() => {
@@ -22,10 +24,18 @@ function ServiceTemplate({ img, title, type, description, price }) {
       className="mt-6 w-72 lg:w-64 xl:w-72 h-[380px] bg-[#f4f5ff] hover:shadow-xl hover:shadow-slate-500 hover:scale-105 duration-300 transition-colors flex flex-col shadow-[0_0_10px_0_rgba(0,0,0,0.25)]"
     >
       <CardHeader className="relative h-48 overflow-hidden m-0 rounded-t-xl min-h-[192px] flex-shrink-0">
-        <img
+        <LazyLoadImage
           src={img}
           alt="card-image"
           className=" w-full h-full object-contain"
+          effect="blur"
+          wrapperProps={{
+            style: {
+              transitionDelay: "1s",
+              width: "100%",
+              height: "100%",
+            }
+          }}
         />
       </CardHeader>
       <CardBody className=" p-2 flex-grow-0">
