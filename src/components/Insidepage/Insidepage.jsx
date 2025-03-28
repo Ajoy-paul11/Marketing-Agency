3983c7b35619064aa6b91c5998026847a72f8475
Ajoy-paul11 <ajoypaul8875@gmail.com>
2025-03-29 00:07:20 +0530

    Refactor Insidepage component to improve modal functionality and enhance image handling.

--- src/components/Insidepage/Insidepage.jsx
+++ src/components/Insidepage/Insidepage.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardHeader,
@@ -15,10 +15,14 @@ import {
 } from "@material-tailwind/react";
 
 function Insidepage({ headerText, image, title, description, users }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState("");
 
-    const openModal = () => setIsModalOpen(true);
-    const closeModal = () => setIsModalOpen(false);
+  const openModal = (modalImg) => {
+    setModalImage(modalImg);
+    setIsModalOpen(true);
+  };
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <section className=" relative w-full py-10">
@@ -42,24 +46,53 @@ function Insidepage({ headerText, image, title, description, users }) {
         </div>
         <div className=" relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-10">
           <div className="mt-6 w-80 shadow-[0px_0px_10px_rgba(0,0,0,0.25)] rounded-lg">
-            <div
-              className=""
-            >
+            <div className="">
               <div>
                 <div className="">
                   <img
                     src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
                     alt="card-image"
                     className={` w-full h-full object-cover rounded-t-lg`}
-                    onClick={openModal}
+                    onClick={() =>
+                      openModal(
+                        "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+                      )
+                    }
                   />
                 </div>
               </div>
             </div>
             <div>
-              <div className="mb-2 text-xl">
-                UI/UX Review Check
+              <div className="mb-2 text-xl">UI/UX Review Check</div>
+              <div>
+                The place is close to Barceloneta Beach and bus stop just 2 min
+                by walk and near to &quot;Naviglio&quot; where you can enjoy the
+                main night life in Barcelona.
               </div>
+            </div>
+            <div className="pt-0">
+              <button>Read More</button>
+            </div>
+          </div>
+          <div className="mt-6 w-80 shadow-[0px_0px_10px_rgba(0,0,0,0.25)] rounded-lg">
+            <div className="">
+              <div>
+                <div className="">
+                  <img
+                    src="https://images.unsplash.com/photo-1742827871492-72428a28106b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="card-image"
+                    className={` w-full h-full object-cover rounded-t-lg`}
+                    onClick={() =>
+                      openModal(
+                        "https://images.unsplash.com/photo-1742827871492-72428a28106b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      )
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="mb-2 text-xl">UI/UX Review Check</div>
               <div>
                 The place is close to Barceloneta Beach and bus stop just 2 min
                 by walk and near to &quot;Naviglio&quot; where you can enjoy the
@@ -73,25 +106,21 @@ function Insidepage({ headerText, image, title, description, users }) {
         </div>
       </div>
       {isModalOpen && (
-        <div 
+        <div
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70"
           onClick={closeModal}
         >
-          <div 
-            className="relative max-w-4xl max-h-[90vh] w-full"
+          <div
+            className="relative max-w-4xl w-full p-[120px]"
             onClick={(e) => e.stopPropagation()}
           >
-            <button 
+            <button
               onClick={closeModal}
               className="absolute -top-10 right-0 text-white text-3xl hover:text-gray-300"
             >
               &times;
             </button>
-            <img 
-              src={image} 
-              alt="" 
-              className="w-full h-full object-contain" 
-            />
+            <img src={modalImage} alt="" className="w-full object-cover" />
           </div>
         </div>
       )}
