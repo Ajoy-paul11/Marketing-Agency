6f05a0afef7bd12754a5227b327079c28a50f709
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-04-01 18:36:14 +0530

    Add Insidepage components for Kannada Prabha, Bangalore and Mid Day, Mumbai with advertising details.

---
+++ src/components/Insidepage/Newspaper/HindustantimesMumbai.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import Insidepage from '../Insidepage'
+import hindustantimesMumbai from "../../../assets/Newspaper/hindustan.jpg"
+
+function HindustantimesMumbai() {
+    const cardInfos = [
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          item: "Custom Sized Ads",
+          about:
+            "Custom Sized Ads are display advertisements that are showcased in newspapers as per the advertiser's requirement. These ads are customizable based on the advertiser's budget and are displayed on the front page, third page, back page, and any pages. Custom Sized Ads have a minimum size of approx. 240sq.cm on the front page and approx. 20sq. cm on all other pages.",
+          price: "3,300 Per Sq Cm",
+        },
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          item: "Quarter Page",
+          about:
+            "Quarter Page Newspaper Ads are display ads that will cover a quarter (1/4) of the newspaper. Quarter Page Ads will take up approx. 400sq. cm for ad space.",
+          price: "13,53,000 Per Insert"
+        },
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          item: "Half Page",
+          about:
+            "Half Page Newspaper Ads will cover half of the page and can be displayed on any page of the newspaper. Half Page Ads can be placed either horizontally or vertically across the page and have a total area of approx. 823 sq. cm of ad space. Compared to Quater Page Ads, Half Page Ads will have more ad space, in which advertisers can customize the content and make them eye-catching to the audience.",
+          price: "27,39,000 Per Insert"
+        },
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          item: "Full Page",
+          about:
+            "Full Page Newspaper Ads cover an entire page of the newspaper. Full Page Ads can be placed anywhere on the newspaper, other than the front page because the front page always carries the latest news content. Full Page Ads will provide advertisers with good brand visibility as it takes up a large area in the newspaper, i.e. approx. 1726sq. cm.",
+          price: "56,95,800 Per Insert"
+        },
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            item: "Jacket Front Side",
+            about:
+              "Jacket Page Ads are extra pages added before the front page of the newspaper. Jacket Front Side Ads are displayed on the front side of the extra page and will include advertisements that will cover an area of approx. 1579.2 sq. cm space, excluding the Masthead/Title Head",
+            price: "96,82,860 Per Insert"
+        },
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            item: "Jacket Back Side",
+            about:
+              "Jacket Back Side Ads will be displayed on the back side of the extra page that is added to the newspaper's front page. Jacket Back Side Ads cover the entire page with a total area of approx. 1726 sq cm. As the availability of Jacket Back Side Ad only is comparatively rare, advertisers prefer to take Jacket Both Side Ads.",
+            price: "96,82,860 Per Insert"
+        },
+        {
+          cardImage:
+            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            item: "Jacket Both Side",
+            about:
+              "Jacket Both Sides Ads will be displayed on the extra page that is added to the front of the newspaper. Advertisers can choose to display their advertisement on both sides of the extra page. Jacket Both Sides will provide maximum visibility and will help increase brand recall.",
+            price: "1,93,65,720 Per Insert"
+        }
+      ];
+      return (
+        <section className=" w-full mt-[72px]">
+          <Insidepage
+            headerText={"Advertising in Hindustan Times, Mumbai"}
+            image={hindustantimesMumbai}
+            title={"About Advertising in Hindustan Times, Mumbai : "}
+            description={
+              "Hindustan Times, Mumbai, English with a circulation of around 625000 is one of the most popular English newspaper. Hindustan Times, Mumbai, English helps build trustworthiness through association covering different topics. This newspaper helps showcase ads to a wide audience, improving visibility and delivering high impact by prompting potential customers in making purchases."
+            }
+            users={"625K Circulation"}
+            cardInfos={cardInfos}
+          />
+        </section>
+      );
+}
+
+export default HindustantimesMumbai
--- src/components/service/NewspaperService.jsx
+++ src/components/service/NewspaperService.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import ServiceTemplate from './ServiceTemplate.jsx';
+import React from "react";
+import ServiceTemplate from "./ServiceTemplate.jsx";
 import economic from "../../assets/Newspaper/economic2.jpg";
 import hindu from "../../assets/Newspaper/hindu2.jpg";
 import toi from "../../assets/Newspaper/TOI.png";
 import hindustan from "../../assets/Newspaper/hindustan2.jpg";
 import nbt from "../../assets/Newspaper/nbt2.jpg";
 import midDay from "../../assets/Newspaper/midday2.jpg";
-import kannada_prabha from "../../assets/Newspaper/kannada-prabha-288-x-192.jpg"
-import prajavani from "../../assets/Newspaper/prajavani-288-x-192.jpg"
-import udayavani from "../../assets/Newspaper/udayavani-288-x-192.jpg"
-import vijaya_karnataka from "../../assets/Newspaper/vijaya-karnataka-288-x-192.jpg"
-import ScrollToTop from '../ScrollToTop.jsx';
-import Category from '../Category.jsx';
+import kannada_prabha from "../../assets/Newspaper/kannada-prabha-288-x-192.jpg";
+import prajavani from "../../assets/Newspaper/prajavani-288-x-192.jpg";
+import udayavani from "../../assets/Newspaper/udayavani-288-x-192.jpg";
+import vijaya_karnataka from "../../assets/Newspaper/vijaya-karnataka-288-x-192.jpg";
+import ScrollToTop from "../ScrollToTop.jsx";
+import Category from "../Category.jsx";
+import { useNavigate } from "react-router-dom";
 
 function NewspaperService() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full mt-[75px]">
       <Category />
@@ -24,86 +27,319 @@ function NewspaperService() {
           Newspaper Services:
         </h2>
         <div className=" grid grid-cols-1 gap-y-4 md:grid-cols-2 lg:grid-cols-3 justify-items-center lg:px-16">
-          <ServiceTemplate
-            img={economic}
-            title="The Economic Times"
-            type="Engage India's business community with targeted financial news ads."
-            description={
-              "45345"
-            }
-            price={"14,860 Min Spend"}
-          />
-          <ServiceTemplate
-            img={hindu}
-            title="The Hindu"
-            type="Reach discerning readers seeking national and international coverage."
-            description={
-              "121432"
-            }
-            price={"25,120 Min Spend"}
-          />
-          <ServiceTemplate
-            img={toi}
-            title="The Times of India"
-            type="Connect with a urban audience through India's largest English daily."
-            description={
-              "366465"
-            }
-            price={"59,040 Min Spend"}
-          />
-          <ServiceTemplate
-            img={hindustan}
-            title="Hindustan Times, Mumbai"
-            type="Target North India's urban populace with comprehensive news coverage."
-            description={"625000"}
-            price={"59,400 Min Spend"}
-          />
-          <ServiceTemplate
-            img={nbt}
-            title="Navbharat Times"
-            type="Engage Hindi-speaking readers in city areas with trusted news content."
-            description={"310978"}
-            price={"35,100 Min Spend"}
-          />
+          <div onClick={() => navigate("/economictimes-bengalore")}>
+            <ServiceTemplate
+              img={economic}
+              title={"Economic Times, Bangalore"}
+              type="Engage India's business community with targeted financial news ads."
+              description={"45345"}
+              price={"14,860 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-mumbai")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Mumbai"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"750000"}
+              price={"1,01,520 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/hindustantimes-delhi")}>
+            <ServiceTemplate
+              img={hindustan}
+              title="Hindustan Times, Delhi"
+              type="Target North India's urban populace with comprehensive news coverage."
+              description={"686418"}
+              price={"1,06,560 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-delhi")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Delhi"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"594773"}
+              price={"74,160 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-bangalore")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Bangalore"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"121432"}
+              price={"25,120 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-hyderabad")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Hyderabad"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"138364"}
+              price={"24,300 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-bangalore")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Bengalore"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"366465"}
+              price={"59,040 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/hindustantimes-mumbai")}>
+            <ServiceTemplate
+              img={hindustan}
+              title="Hindustan Times, Mumbai"
+              type="Target North India's urban populace with comprehensive news coverage."
+              description={"625000"}
+              price={"59,400 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/navbharat-delhi")}>
+            <ServiceTemplate
+              img={nbt}
+              title="Navbharat Times, Delhi"
+              type="Engage Hindi-speaking readers in city areas with trusted news content."
+              description={"310978"}
+              price={"35,100 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/economictimes-mumbai")}>
+            <ServiceTemplate
+              img={economic}
+              title={"Economic Times, Mumbai"}
+              type="Engage India's business community with targeted financial news ads."
+              description={"195000"}
+              price={"51,200 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/economictimes-delhi")}>
+            <ServiceTemplate
+              img={economic}
+              title={"Economic Times, Delhi"}
+              type="Engage India's business community with targeted financial news ads."
+              description={"86621"}
+              price={"46,900 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-delhi")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Delhi"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"104720"}
+              price={"13,780 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-pune")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Pune"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"123561"}
+              price={"43,660 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-chennai")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Chennai"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"197688"}
+              price={"1,02,880 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/prajavani-bangalore")}>  
           <ServiceTemplate
             img={prajavani}
-            title="Prajavani"
+            title="Prajavani, Bangalore"
             type="Reach Kannada-speaking audiences through Karnataka's leading daily."
             description={"156000"}
             price={"23,400 Min Spend"}
-          />
+            />
+          </div>
+          <div onClick={() => navigate("/vijayakarnataka-bangalore")}>
           <ServiceTemplate
             img={vijaya_karnataka}
-            title="Vijaya Karnataka"
+            title="Vijaya Karnataka, Bangalore"
             type="Focus a vast readership in Karnataka's prominent newspaper."
             description={"4,91,200"}
             price={"61,560 Min Spend"}
-          />
+            />
+          </div>
+          <div onClick={() => navigate("/udayavani-bangalore")}>
           <ServiceTemplate
             img={udayavani}
-            title="Udayavani"
+            title="Udayavani, Bangalore"
             type="Connect with coastal Karnataka's readers via circulated Kannada daily."
             description={"300000"}
             price={"17,600 Min Spend"}
-          />
+            />
+          </div>
+          <div onClick={() => navigate("/kannadaprabha-bangalore")}>
           <ServiceTemplate
             img={kannada_prabha}
-            title="Kannada Prabha"
+            title="Kannada Prabha, Bangalore"
             type="Target Kannada readers with top journalism and regional news."
             description={"45000"}
             price={"11,880 Min Spend"}
-          />
-          <ServiceTemplate
-            img={midDay}
-            title="Mid-Day"
-            type="Engage Mumbai's urban audience with this popular afternoon daily."
-            description={"300000"}
-            price={"39,422 Min Spend"}
-          />
+            />
+          </div>
+          <div onClick={() => navigate("/midday-mumbai")}>
+            <ServiceTemplate
+              img={midDay}
+              title="Mid-Day, Mumbai"
+              type="Engage Mumbai's urban audience with this popular afternoon daily."
+              description={"300000"}
+              price={"39,422 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-noida")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Noida"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"17049"}
+              price={"6,660 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/navbharat-mumbai")}>
+            <ServiceTemplate
+              img={nbt}
+              title="Navbharat Times, Mumbai"
+              type="Engage Hindi-speaking readers in city areas with trusted news content."
+              description={"47380"}
+              price={"19,960 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-chennai")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Chennai"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"1,91,000"}
+              price={"71,460 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-kolkata")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Kolkata"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"173265"}
+              price={"33,040 Min Spend"}
+            />
+          </div>
+          
+          <div onClick={() => navigate("/economicstimes-chennai")}>
+            <ServiceTemplate
+              img={economic}
+              title="Economic Times, Chennai"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"20600"}
+              price={"14,140 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/timesofindia-ahmedabad")}>
+            <ServiceTemplate
+              img={toi}
+              title="Times of India, Ahmedabad"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"123956"}
+              price={"13,320 Min Spend"}
+            />
+          </div>
+          
+          <div onClick={() => navigate("/economicstimes-kolkata")}>
+            <ServiceTemplate
+              img={economic}
+              title="Economic Times, Kolkata"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"22671"}
+              price={"12,880 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/economicstimes-hyderabad")}>
+            <ServiceTemplate
+              img={economic}
+              title="Economic Times, Hyderabad"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"15879"}
+              price={"8,200 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/economicstimes-pune")}>
+            <ServiceTemplate
+              img={economic}
+              title="Economic Times, Pune"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"20255"}
+              price={"6,760 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-coimbatore")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Coimbatore"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"77295"}
+              price={"19,000 Min Spend"}
+            />
+          </div>
+          
+          <div onClick={() => navigate("/thehindu-mumbai")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Mumbai"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"37091"}
+              price={"8,740 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-hyderabad")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Hyderabad"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"135707"}
+              price={"20,620 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/economicstimes-kochi")}>
+            <ServiceTemplate
+              img={economic}
+              title="Economic Times, Kochi"
+              type="Connect with a urban audience through India's largest English daily."
+              description={"5485"}
+              price={"1,560 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-mangaluru")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Mangaluru"
+              type="Reach discerning readers seeking national and international coverage."
+              description={"12634"}
+              price={"2,400 Min Spend"}
+            />
+          </div>
+          <div onClick={() => navigate("/thehindu-madurai")}>
+            <ServiceTemplate
+              img={hindu}
+              title="The Hindu, Mangaluru"
+              type="Reach discerning readers seeking national and international coverage."
+              description="50,567"
+              price={"8,760"}
+            />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default NewspaperService
+export default NewspaperService;
