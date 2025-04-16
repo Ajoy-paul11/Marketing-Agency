fdee38c156916312c7a31cf22f3f9d8c90d97c63
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-04-16 16:16:24 +0530

    Update HinduMangaluru and HinduMadurai components with new images and improved ad descriptions for better clarity and engagement.

--- src/components/Insidepage/Newspaper/HinduMangaluru.jsx
+++ src/components/Insidepage/Newspaper/HinduMangaluru.jsx
@@ -1,49 +1,50 @@
-import React from 'react'
-import Insidepage from '../Insidepage'
-import hinduMangaluru from '../../../assets/Newspaper/hindu.jpg'
+import React from "react";
+import Insidepage from "../Insidepage";
+import hinduMangaluru from "../../../assets/Newspaper/hindu.jpg";
+import ScrollToTop from "../../ScrollToTop";
 
 function HinduMangaluru() {
-    const cardInfos = [
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          item: "Custom Sized Ads",
-          about:
-            "Custom Sized Ads are display advertisements that are showcased in newspapers as per the advertiser's requirement. These ads are customizable based on the advertiser's budget and are displayed on the front page, third page, back page, and any pages. Custom Sized Ads have a minimum size of approx. 240sq.cm on the front page and approx. 20sq. cm on all other pages.",
-          price: " 150 Per Sq Cm",
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          item: "Quarter Page",
-          about:
-            "Quarter Page Newspaper Ads are display ads that will cover a quarter (1/4) of the newspaper. Quarter Page Ads will take up approx. 400sq. cm for ad space.",
-          price: "60,000 Per Insert"
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            item: "Jacket Front Side",
-            about:
-              "Jacket Page Ads are extra pages added before the front page of the newspaper. Jacket Front Side Ads are displayed on the front side of the extra page and will include advertisements that will cover an area of approx. 1579.2 sq. cm space, excluding the Masthead/Title Head",
-            price: "3,21,750 Per Insert"
-        },
-        
-      ];
-      return (
-        <section className=" w-full mt-[72px]">
-          <Insidepage
-            headerText={"Advertising in The Hindu, Mangaluru"}
-            image={hinduMangaluru}
-            title={"About Advertising in The Hindu, Mangaluru : "}
-            description={
-              "The Hindu, Mangaluru, English with a circulation of around 12634 is one of the most popular English newspaper. The Hindu, Mangaluru, English helps build trustworthiness through association covering different topics. This newspaper helps showcase ads to a wide audience, improving visibility and delivering high impact by prompting potential customers in making purchases."
-            }
-            users={"12.6K Circulation"}
-            cardInfos={cardInfos}
-          />
-        </section>
-      );
+  const cardInfos = [
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/newspaper-cusom-ad2.jpg?updatedAt=1744783526936",
+      item: "Custom Sized Ads",
+      about:
+        "Custom Sized Ads are display advertisements that are showcased in newspapers as per the advertiser's requirement. These ads are customizable based on the advertiser's budget and are displayed on the front page, third page, back page, and any pages. Custom Sized Ads have a minimum size of approx. 240sq.cm on the front page and approx. 20sq. cm on all other pages.",
+      price: " 150 Per Sq Cm",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-quarter-page.jpg?updatedAt=1744782773705",
+      item: "Quarter Page",
+      about:
+        "Quarter Page Newspaper Ads are display ads that will cover a quarter (1/4) of the newspaper. Quarter Page Ads will take up approx. 400sq. cm for ad space.",
+      price: "60,000 Per Insert",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-jacket-frontpage.jpg?updatedAt=1744782772607",
+      item: "Jacket Front Side",
+      about:
+        "Jacket Page Ads are additional pages inserted before the newspaper’s front page. The front side of the jacket carries Jacket Front Side Ads, which typically occupy around 1,579.2 sq. cm of space, excluding the Masthead or Title Head.",
+      price: "3,21,750 Per Insert",
+    },
+  ];
+  return (
+    <section className=" w-full mt-[72px]">
+      <ScrollToTop />
+      <Insidepage
+        headerText={"Advertising in The Hindu, Mangaluru"}
+        image={hinduMangaluru}
+        title={"About Advertising in The Hindu, Mangaluru : "}
+        description={
+          "The Hindu, Mangaluru, English with a circulation of around 12634 is one of the most popular English newspaper. The Hindu, Mangaluru, English helps build trustworthiness through association covering different topics. This newspaper helps showcase ads to a wide audience, improving visibility and delivering high impact by prompting potential customers in making purchases."
+        }
+        users={"12.6K Circulation"}
+        cardInfos={cardInfos}
+      />
+    </section>
+  );
 }
 
-export default HinduMangaluru
+export default HinduMangaluru;
--- src/components/Insidepage/Newspaper/MiddayMumbai.jsx
+++ src/components/Insidepage/Newspaper/MiddayMumbai.jsx
@@ -1,80 +1,82 @@
-import React from 'react'
-import Insidepage from '../Insidepage'
-import midday from '../../../assets/Newspaper/midday.jpg'
+import React from "react";
+import Insidepage from "../Insidepage";
+import midday from "../../../assets/Newspaper/midday.jpg";
+import ScrollToTop from "../../ScrollToTop";
 
 function MiddayMumbai() {
-    const cardInfos = [
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          item: "Custom Sized Ads",
-          about:
-            "Custom Sized Ads are display advertisements that are showcased in newspapers as per the advertiser's requirement. These ads are customizable based on the advertiser's budget and are displayed on the front page, third page, back page, and any pages. Custom Sized Ads have a minimum size of approx. 240sq.cm on the front page and approx. 20sq. cm on all other pages.",
-          price: "2,277 Per Sq Cm",
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          item: "Quarter Page",
-          about:
-            "Quarter Page Newspaper Ads are display ads that will cover a quarter (1/4) of the newspaper. Quarter Page Ads will take up approx. 400sq. cm for ad space.",
-          price: "4,09,860 Per Insert"
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          item: "Half Page",
-          about:
-            "Half Page Newspaper Ads will cover half of the page and can be displayed on any page of the newspaper. Half Page Ads can be placed either horizontally or vertically across the page and have a total area of approx. 823 sq. cm of ad space. Compared to Quater Page Ads, Half Page Ads will have more ad space, in which advertisers can customize the content and make them eye-catching to the audience.",
-          price: "9,10,800 Per Insert"
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          item: "Full Page",
-          about:
-            "Full Page Newspaper Ads cover an entire page of the newspaper. Full Page Ads can be placed anywhere on the newspaper, other than the front page because the front page always carries the latest news content. Full Page Ads will provide advertisers with good brand visibility as it takes up a large area in the newspaper, i.e. approx. 1726sq. cm.",
-          price: "18,28,431 Per Insert"
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            item: "Jacket Front Side",
-            about:
-              "Jacket Page Ads are extra pages added before the front page of the newspaper. Jacket Front Side Ads are displayed on the front side of the extra page and will include advertisements that will cover an area of approx. 1579.2 sq. cm space, excluding the Masthead/Title Head",
-            price: "36,56,862 Per Insert"
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            item: "Jacket Back Side",
-            about:
-              "Jacket Back Side Ads will be displayed on the back side of the extra page that is added to the newspaper's front page. Jacket Back Side Ads cover the entire page with a total area of approx. 1726 sq cm. As the availability of Jacket Back Side Ad only is comparatively rare, advertisers prefer to take Jacket Both Side Ads.",
-            price: "36,56,862 Per Insert"
-        },
-        {
-          cardImage:
-            "https://images.pexels.com/photos/9832700/pexels-photo-9832700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            item: "Jacket Both Side",
-            about:
-              "Jacket Both Sides Ads will be displayed on the extra page that is added to the front of the newspaper. Advertisers can choose to display their advertisement on both sides of the extra page. Jacket Both Sides will provide maximum visibility and will help increase brand recall.",
-            price: "73,13,724 Per Insert"
+  const cardInfos = [
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/newspaper-cusom-ad2.jpg?updatedAt=1744783526936",
+      item: "Custom Sized Ads",
+      about:
+        "Custom Sized Ads are display advertisements that are showcased in newspapers as per the advertiser's requirement. These ads are customizable based on the advertiser's budget and are displayed on the front page, third page, back page, and any pages. Custom Sized Ads have a minimum size of approx. 240sq.cm on the front page and approx. 20sq. cm on all other pages.",
+      price: "2,277 Per Sq Cm",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-quarter-page.jpg?updatedAt=1744782773705",
+      item: "Quarter Page",
+      about:
+        "Quarter Page Newspaper Ads are display ads that will cover a quarter (1/4) of the newspaper. Quarter Page Ads will take up approx. 400sq. cm for ad space.",
+      price: "4,09,860 Per Insert",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-half-page.webp?updatedAt=1744782772431",
+      item: "Half Page",
+      about:
+        "Half Page Newspaper Ads will cover half of the page and can be displayed on any page of the newspaper. Half Page Ads can be placed either horizontally or vertically across the page and have a total area of approx. 823 sq. cm of ad space. Compared to Quater Page Ads, Half Page Ads will have more ad space, in which advertisers can customize the content and make them eye-catching to the audience.",
+      price: "9,10,800 Per Insert",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-full-page.webp?updatedAt=1744782773696",
+      item: "Full Page",
+      about:
+        "Full Page Newspaper Ads occupy an entire page of the newspaper. These ads can be placed on any page except the front page, as it is reserved for the latest news content. With a large ad space of approximately 1,726 sq. cm, Full Page Ads offer advertisers excellent brand visibility.",
+      price: "18,28,431 Per Insert",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-jacket-frontpage.jpg?updatedAt=1744782772607",
+      item: "Jacket Front Side",
+      about:
+        "Jacket Page Ads are additional pages inserted before the newspaper’s front page. The front side of the jacket carries Jacket Front Side Ads, which typically occupy around 1,579.2 sq. cm of space, excluding the Masthead or Title Head.",
+      price: "36,56,862 Per Insert",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-jacket-backpage.jpg?updatedAt=1744782772696",
+      item: "Jacket Back Side",
+      about:
+        "Jacket Back Side Ads will be displayed on the back side of the extra page that is added to the newspaper's front page. Jacket Back Side Ads cover the entire page with a total area of approx. 1726 sq cm. As the availability of Jacket Back Side Ad only is comparatively rare, advertisers prefer to take Jacket Both Side Ads.",
+      price: "36,56,862 Per Insert",
+    },
+    {
+      cardImage:
+        "https://ik.imagekit.io/ajoy/Newspaper/news-jacket-fullpage.jpg?updatedAt=1744782772478",
+      item: "Jacket Both Side",
+      about:
+        "Jacket Both Sides Ads are featured on the extra page placed at the front of the newspaper, with advertisements displayed on both the front and back sides. This format offers maximum visibility and helps enhance brand recall for advertisers.",
+      price: "73,13,724 Per Insert",
+    },
+  ];
+  return (
+    <section className=" w-full mt-[72px]">
+      <ScrollToTop />
+      <Insidepage
+        headerText={"Advertising in Mid Day, Mumbai"}
+        image={midday}
+        title={"About Advertising in Mid Day, Mumbai : "}
+        description={
+          "Mid Day, Mumbai, English with a circulation of around 300000 is one of the most popular English newspaper. Mid Day, Mumbai, English helps build trustworthiness through association covering different topics. This newspaper helps showcase ads to a wide audience, improving visibility and delivering high impact by prompting potential customers in making purchases."
         }
-      ];
-      return (
-        <section className=" w-full mt-[72px]">
-          <Insidepage
-            headerText={"Advertising in Mid Day, Mumbai"}
-            image={midday}
-            title={"About Advertising in Mid Day, Mumbai : "}
-            description={
-              "Mid Day, Mumbai, English with a circulation of around 300000 is one of the most popular English newspaper. Mid Day, Mumbai, English helps build trustworthiness through association covering different topics. This newspaper helps showcase ads to a wide audience, improving visibility and delivering high impact by prompting potential customers in making purchases."
-            }
-            users={"300K Circulation"}
-            cardInfos={cardInfos}
-          />
-        </section>
-      );
+        users={"300K Circulation"}
+        cardInfos={cardInfos}
+      />
+    </section>
+  );
 }
 
-export default MiddayMumbai
+export default MiddayMumbai;
