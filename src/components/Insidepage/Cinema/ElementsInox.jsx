cf297c3f999c6ce66a065a947a694e98e354932d
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-04-17 15:07:34 +0530

    Refine ad descriptions in GarudaMall, SwagathMall, NagavaraInox, OrienMallInox, SoulSpaceInox, AshoknagarInox, and YeswantpurInox components for improved clarity and engagement.

--- src/components/Insidepage/Cinema/ElementsInox.jsx
+++ src/components/Insidepage/Cinema/ElementsInox.jsx
@@ -9,7 +9,7 @@ function ElementsInox() {
             "https://images.unsplash.com/photo-1517486518908-97a5f91b325f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           item: "Slide Ad - On Screen",
           about:
-            "In Slide ads one can advertise via an image creative with or without audio. Slide ads can be played either before the movie begins, during the movie interval or both. The duration of slide ads are in multiples of 10 seconds.",
+            "Slide ads allow advertising through image creatives with or without audio. These ads can be shown before the movie starts, during the interval, or both. The duration of slide ads is in 10-second increments.",
           price: "600 Per Second",
         },
         {
@@ -17,7 +17,7 @@ function ElementsInox() {
             "https://images.unsplash.com/photo-1517486518908-97a5f91b325f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           item: "Video Ad - On Screen",
           about:
-            "In Video Ads, one can advertise via a video creative. Video ads can be played either before the movie begins, during the movie interval or both. The duration of video ads are in multiples of 10 seconds.",
+            "Video Ads allow advertising through video creatives. These ads can be played either before the movie starts, during the interval, or both. The duration of video ads is in 10-second increments.",
           price: "600 Per Second",
         },
       ];
