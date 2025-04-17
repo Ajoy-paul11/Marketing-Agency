79e82d7c4ec1558daeee14460fdc6504946896f6
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-04-17 11:56:48 +0530

    Refactor jingle descriptions in multiple components for improved clarity and consistency.

--- src/components/Insidepage/MirchiHyderbad.jsx
+++ src/components/Insidepage/MirchiHyderbad.jsx
@@ -9,7 +9,7 @@ function MirchiHyderbad() {
             "https://images.unsplash.com/photo-1478737270239-2f02b77fc618?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           item: "Jingle",
           about:
-            "Jingle is a short audio message, used to define and promote a product or a brand. It usually varies between 10-30 seconds. It can be played during between 7am - 12pm & 5pm - 11pm, Mixed Time - Ads are split between 7am - 11pm.",
+            "A jingle is a short audio message designed to define and promote a product or brand, typically lasting between 10 to 30 seconds. It can be broadcast between 7 AM – 12 PM and 5 PM – 11 PM. Mixed Time slots allow ads to be aired anytime between 7 AM – 11 PM.",
           price: "44 Per Second",
         },
       ];
