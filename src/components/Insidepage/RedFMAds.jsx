440b0feaf1d05cd26fa1229702297f55012c3db5
Ajoy Paul <ajoypaulmindblue@gmail.com>
2025-04-18 12:59:05 +0530

    Update jingle images across multiple components for enhanced visual consistency.

--- src/components/Insidepage/RedFMAds.jsx
+++ src/components/Insidepage/RedFMAds.jsx
@@ -6,7 +6,7 @@ function RedFMAds() {
     const cardInfos = [
         {
           cardImage:
-            "https://images.unsplash.com/photo-1478737270239-2f02b77fc618?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://ik.imagekit.io/ajoy/Radio/radio-jingles-service.jpg?updatedAt=1744960871788",
           item: "Jingle",
           about:
             "A jingle is a short audio message designed to define and promote a product or brand, typically lasting between 10 to 30 seconds. It can be broadcast between 7 AM – 12 PM and 5 PM – 11 PM. Mixed Time slots allow ads to be aired anytime between 7 AM – 11 PM.",
